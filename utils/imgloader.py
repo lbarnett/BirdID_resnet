a72a2676fde358856387482f6f683fb69eec2222
lbarnett <lbarnett@richmond.edu>
2019-02-25 11:42:02 -0500

    Added to imgloader.py splitting code a choice of moving files or     copying them. The later is useful if you want to preserve the     original folder for trying different random splits or splits     with different train/validate proportions. Added some docstrings,     some small formatting changes.

--- utils/imgloader.py
+++ utils/imgloader.py
@@ -1,6 +1,9 @@
+"""Utilities for loading images."""
+
 import os
-import shutil
 import re
+import shutil
+
 import multiprocessing
 
 import numpy as np
@@ -12,11 +15,12 @@ from .imgprocessing import resize_and_crop
 
 def _split_data(X, y, p_train=0.5, seed=None):
     """
-    Splits data into train and test data.
+    Split data into train and test data.
+
     X contains the data and y contains the labels.
     """
-    sss = StratifiedShuffleSplit(n_splits=1, test_size=None, train_size=p_train,
-                                 random_state=seed)
+    sss = StratifiedShuffleSplit(n_splits=1, test_size=None,
+                                 train_size=p_train, random_state=seed)
 
     train_index, test_index = next(iter(sss.split(X, y)))
     X_train, X_test = X[train_index], X[test_index]
@@ -39,12 +43,14 @@ def _load_img(path, n_class, new_size=None, scale=True, mode='RGB'):
 
 def _multi_load_img(paths, labels, new_size=None, scale=True):
     """
-    Loads images from paths asynchronously. Also preserves the labels.
-    """
+    Load images from paths asynchronously.
 
+    Also preserves the labels.
+    """
     pool = multiprocessing.Pool()
     num_samples = len(paths)
-    results = [pool.apply_async(_load_img, (paths[i], labels[i], new_size, scale)) 
+    results = [pool.apply_async(_load_img, (paths[i], labels[i],
+                                new_size, scale))
                for i in xrange(num_samples)]
     imgs = [r.get() for r in results]
 
@@ -77,6 +83,7 @@ def get_paths_with_labels(folder):
         A tuple of file paths, corresponding labels, and label names (
         label names are just the names of the subdirectories, in the order
         they were loaded in).
+
     """
     filepaths = []
     categories = []
@@ -84,16 +91,20 @@ def get_paths_with_labels(folder):
     label_names = []  # class num to class name
 
     n_class = 0
-    # get the file paths
+    # get the file paths. root is a subfolder found recursively,
+    # dirnames is the list of subdirectories
+    # of root, and filenames is the list of regular files in root.
     for root, dirnames, filenames in os.walk(folder):
         dirnames.sort()
+
+        # Skip top level folder
         if root == folder:
             continue
 
         class_id = root.split('/')[-1]
 
         for filename in filenames:
-            if re.search('(?i)\.(jpg|png|jpeg)$', filename):
+            if re.search(r'(?i)\.(jpg|png|jpeg)$', filename):
                 filepath = os.path.join(root, filename)
                 filepaths.append(filepath)
                 categories.append(n_class)
@@ -104,13 +115,13 @@ def get_paths_with_labels(folder):
     return filepaths, categories, label_names
 
 
-def load_data(folder, p_train=0.5, new_size=None, 
+def load_data(folder, p_train=0.5, new_size=None,
               scale=True, seed=None):
     """
-    Loads data from a folder and returns the tuple (train, test, label_names).
+    Load data from a folder and returns the tuple (train, test, label_names).
+
     Expects the folder to have a subdirectory per class.
     """
-
     paths, labels, label_names = get_paths_with_labels(folder)
 
     paths = np.asarray(paths)
@@ -122,21 +133,23 @@ def load_data(folder, p_train=0.5, new_size=None,
         test = None
     else:
         train, test = _split_data(paths, labels, p_train, seed)
-        train = _multi_load_img(train[0], train[1], new_size=new_size, scale=scale)
-        test = _multi_load_img(test[0], test[1], new_size=new_size, scale=scale)
+        train = _multi_load_img(train[0], train[1],
+                                new_size=new_size, scale=scale)
+        test = _multi_load_img(test[0], test[1],
+                               new_size=new_size, scale=scale)
 
-    print train[0].shape    
+    print train[0].shape
 
     return train, test, label_names
 
 
 def _move_data(folder, paths, labels, label_names):
-    """Move files to a given folder.
+    """
+    Move files to a given folder.
 
     Helper function for distributing data into subdirectories based on
     their labels.
     """
-
     for i in xrange(len(paths)):
         p, label = paths[i], labels[i]
         subfolder = os.path.join(folder, label_names[label])
@@ -147,15 +160,39 @@ def _move_data(folder, paths, labels, label_names):
         shutil.move(p, subfolder)
 
 
-def split_train_val(folder, p_train=0.5, seed=None):
+def _copy_data(folder, paths, labels, label_names):
+    """Move files to a given folder.
 
+    Helper function for distributing data into subdirectories based on
+    their labels.
+    """
+    for i in xrange(len(paths)):
+        p, label = paths[i], labels[i]
+        subfolder = os.path.join(folder, label_names[label])
+
+        if not os.path.exists(subfolder):
+            os.makedirs(subfolder)
+
+        shutil.copy(p, subfolder)
+
+
+def split_train_val(folder, p_train=0.5, seed=None, mode="move"):
+    """Split data files into training and validation sets."""
     paths, labels, label_names = get_paths_with_labels(folder)
 
     paths = np.asarray(paths)
     labels = np.asarray(labels)
 
-    (train_x, train_y), (val_x, val_y) = _split_data(paths, labels,
-                                                     p_train=p_train, seed=seed)
+    (train_x, train_y), (val_x, val_y) = \
+        _split_data(paths, labels, p_train=p_train, seed=seed)
 
-    _move_data(os.path.join(folder, 'train'), train_x, train_y, label_names)
-    _move_data(os.path.join(folder, 'val'), val_x, val_y, label_names)
+    if mode == "move":
+        _move_data(os.path.join(folder, 'train'),
+                   train_x, train_y, label_names)
+        _move_data(os.path.join(folder, 'val'),
+                   val_x, val_y, label_names)
+    else:
+        _copy_data(os.path.join(folder, 'train'),
+                   train_x, train_y, label_names)
+        _copy_data(os.path.join(folder, 'val'),
+                   val_x, val_y, label_names)
